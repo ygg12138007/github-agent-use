066aa954c24cfb48d2739dfe8243b7d052d0457e
ygg12138win <ygg12138@163.com>
2025-09-30 14:11:23 +0800

    feat: initialize project and remove framework examples

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// 基础应用组件
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-container">
+    <h1>EMS 应用</h1>
+    <p>项目已成功初始化</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app-container {
+  text-align: center;
+  padding: 20px;
 }
 </style>
